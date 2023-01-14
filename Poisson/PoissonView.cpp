3ee80e2b797d1f728b768c7d0c2faf38b7e00314
Adrian Roman <aroman_ro@yahoo.com>
2023-01-14 17:35:57 +0200

    Switched to VTK 9.2.5

--- Poisson/PoissonView.cpp
+++ Poisson/PoissonView.cpp
@@ -143,7 +143,7 @@ void CPoissonView::OnDraw(CDC* pDC)
 		renWin->SetUseOffScreenBuffers(true);
 		renWin->Render();
 		
-		const unsigned char *pixels = renWin->GetPixelData(0,0,cxWindow-1,cyWindow-1,0);
+		const unsigned char *pixels = renWin->GetPixelData(0,0,cxWindow-1,cyWindow-1,0, NULL);
 
 		int dataWidth = ((cxWindow*3+3)/4)*4;
 
